3a5331c55a56af0f688ab2ab6b19640083198f37
tekthree <tekthree.gmail.com>
2021-08-10 20:21:26 -0700

    adds todo list functionality, styling

--- App.js
+++ App.js
@@ -1,12 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, {useState} from "react";
+import { KeyboardAvoidingView, Platform, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard } from "react-native";
+import Task from './components/Task';
+
 
 export default function App() {
+  const [task, setTask] =useState();
+  const [taskArr, setTaskArr] = useState([]);
+
+  const taskHandler = () => {
+    Keyboard.dismiss();
+    setTaskArr([...taskArr, task]);
+    setTask(null);
+  }
+
+  const taskDeleter = (index) =>{
+    let itemsCopy = [...taskArr];
+    itemsCopy.splice(index, 1);
+    setTaskArr(itemsCopy);
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskWrapper}>
+        <Text style={styles.sectionTitle}>Todo List</Text>
+
+        <View style={styles.items}>
+        
+        {
+          taskArr.map((item, index)=>{
+            return(
+              <TouchableOpacity key={index} onPress={()=> taskDeleter(index)}>
+                <Task  text={item}></Task>
+              </TouchableOpacity>
+              
+            ) 
+          })
+        }
+        </View>
+
+      </View>
+
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={'input a task'}
+          value={task}
+          onChangeText={text => setTask(text)}
+        ></TextInput>
+        <TouchableOpacity onPress={()=> taskHandler()} >
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>➕</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+
+      
     </View>
   );
 }
@@ -14,8 +67,46 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#E8EAED",
   },
+  taskWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontWeight: "bold",
+    fontSize: 24
+  },
+  items:{
+    marginTop: 30
+  },
+  writeTaskWrapper:{
+    position: "absolute",
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
+  },
+  input:{
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: 'white',
+    borderRadius: 60,
+    borderColor: 'lightgrey',
+    borderWidth: 1,
+    width: 250,
+
+  },
+  addWrapper:{
+    width: 60,
+    height: 60,
+    backgroundColor: "white",
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: 'lightgrey',
+    borderWidth: 1,
+  },
+  addText:{},
 });
---
+++ README.md
@@ -0,0 +1,27 @@
+# React Native
+# lab-41
+## Author: Tek Jones
+## Deployment and test
+  * [test report]()
+  * [deployment lab41]()
+## About
+- Create your first native phone/device application using React Native, tapping into at least 2 phone features.
+## Setup
+## Running the app
+  * npm run android
+## Test
+* npm test
+### Session 1
+- Over the next 2 lab coding sessions, you will create your first Phone App. What it does and how it looks is up to you. This is an opportunity to play around with whats possible in React Native and to get a feel for how you can get into the various device features.
+This is a 2-session assignment, after which you will live-demo your phone app to the class (at the start of class 43
+### User Stories
+### Requirements
+- Use at least 1 native device feature (Contacts, Camera, GPS, etc)
+- High Fidelity Styling and UX
+   - Good starting points:
+     - Native Base
+     - Builder X
+- Properly Documented for End Users
+- Live Demo & Presentation
+## UML
+![UML]()
